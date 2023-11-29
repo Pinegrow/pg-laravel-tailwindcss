2c477cd4f003eca58b4972fbbe4359ca1681b98a
Akayy <techakayy@gmail.com>
2023-11-29 21:12:09 +1100

    05 Lint & Formatted all files in the resources folder

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -1,36 +1,35 @@
 <script setup>
-import { computed } from 'vue';
+  import { computed } from 'vue'
 
-const emit = defineEmits(['update:checked']);
+  const emit = defineEmits(['update:checked'])
 
-const props = defineProps({
+  const props = defineProps({
     checked: {
-        type: [Array, Boolean],
-        default: false,
+      type: [Array, Boolean],
+      default: false,
     },
     value: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
-});
+  })
 
-const proxyChecked = computed({
+  const proxyChecked = computed({
     get() {
-        return props.checked;
+      return props.checked
     },
 
     set(val) {
-        emit('update:checked', val);
+      emit('update:checked', val)
     },
-});
+  })
 </script>
 
-
 <template>
-    <input
-        v-model="proxyChecked"
-        type="checkbox"
-        :value="value"
-        class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
-    >
+  <input
+    v-model="proxyChecked"
+    type="checkbox"
+    :value="value"
+    class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+  />
 </template>
